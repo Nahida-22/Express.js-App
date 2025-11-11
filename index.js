a059a704c8bd62c9796119c2063aef9cbf3890db
Nahida Rojoa <nr681@live.mdx.ac.uk>
2025-11-11 18:04:50 +0400

    Set up mongodb connection

---
+++ index.js
@@ -0,0 +1,55 @@
+var express = require("express");
+let app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+app.set('json spaces', 3);
+const path = require('path');
+let PropertiesReader = require("properties-reader");
+
+// Load properties from the file
+let propertiesPath = path.resolve(__dirname, "./dbconnection.properties");
+let properties = PropertiesReader(propertiesPath);
+
+// Extract values from the properties file
+const dbPrefix = properties.get('db.prefix');
+const dbHost = properties.get('db.host');
+const dbName = properties.get('db.name');
+const dbUser = properties.get('db.user');
+const dbPassword = properties.get('db.password');
+const dbParams = properties.get('db.params');
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// MongoDB connection URL
+const uri = `${dbPrefix}${dbUser}:${dbPassword}${dbHost}${dbParams}`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+
+let db1;//declare variable
+
+async function connectDB() {
+  try {
+    client.connect();
+    console.log('Connected to MongoDB');
+    db1 = client.db('Pet');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+  }
+}
+
+connectDB(); //call the connectDB function to connect to MongoDB database
+
+
+app.param('collectionName', async function(req, res, next, collectionName) { 
+    req.collection = db1.collection(collectionName);
+    /*Check the collection name for debugging if error */
+    console.log('Middleware set collection:', req.collection.collectionName);
+    next();
+});
+
+
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
